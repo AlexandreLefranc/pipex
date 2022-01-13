9beb3e8ecccfb1dd25f904703685a8de16f97dec
Alexandre LEFRANC <alefranc@e1r5p15.42.fr>
2022-01-13 17:53:09 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pipex
+
+# Help
+
+- [Help](https://n-pn.fr/t/2318-c--programmation-systeme-execve-fork-et-pipe)
+- []()	
---
+++ example_execve.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+
+
+// gccc example_execve.c && ./a.out /bin/ls -l
+
+int main(int argc, char *argv[], char *env[])
+{
+
+	printf("%s\n", env[0]);
+
+	printf("My pid is: %d\n", getpid());
+
+	if (argc > 1)
+		if (execve(argv[1], argv + 1, env) == -1)
+			perror("execve yo ");
+
+	return 0;
+}
